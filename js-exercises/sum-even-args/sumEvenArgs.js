9d73d8a48f8fc787657a1516e3042425445aae66
Amit Kaushik <amit.kaushik@mobilecoderz.com>
2020-05-24 09:08:30 +0530

    Sum Even Args Solution improved

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,9 +1,8 @@
-function getSumEvenArgs(sum, num) {
+const sumEvenArgs = (...args) => args.reduce((sum, num) => {
   if (sum % 2 === 0) {
     return num % 2 === 0 ? sum + num : sum;
   }
   return num % 2 === 0 ? num : 0;
-}
-const sumEvenArgs = (...args) => args.reduce(getSumEvenArgs);
+});
 
 export { sumEvenArgs };
