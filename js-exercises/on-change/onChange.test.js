538dd17c59eb272a68e2469504b6be5c36a52271
Amit Kaushik <kaushikabhi999@gmail.com>
2020-06-11 10:27:11 +0530

    Uncomment the commented test case in onChange.test.js

--- js-exercises/on-change/onChange.test.js
+++ js-exercises/on-change/onChange.test.js
@@ -1,47 +1,47 @@
 import { onChange } from './onChange';
 
 describe('onChange', () => {
-  // test('main', () => {
-  //   const fixture = {
-  //     foo: false,
-  //     bar: {
-  //       a: {
-  //         b: 0,
-  //         c: [1, 2],
-  //       },
-  //     },
-  //   };
+  test('main', () => {
+    const fixture = {
+      foo: false,
+      bar: {
+        a: {
+          b: 0,
+          c: [1, 2],
+        },
+      },
+    };
 
-  //   let callCount = 0;
+    let callCount = 0;
 
-  //   const object = onChange(fixture, () => {
-  //     callCount += 1;
-  //   });
+    const object = onChange(fixture, () => {
+      callCount += 1;
+    });
 
-  //   object.foo = true;
-  //   expect(callCount).toBe(1);
+    object.foo = true;
+    expect(callCount).toBe(1);
 
-  //   Object.defineProperty(object, 'newProp', {
-  //     value: '🦄',
-  //   });
-  //   expect(callCount).toBe(2);
+    Object.defineProperty(object, 'newProp', {
+      value: '🦄',
+    });
+    expect(callCount).toBe(2);
 
-  //   Object.assign(object, {
-  //     foo: false,
-  //   });
-  //   expect(callCount).toBe(3);
+    Object.assign(object, {
+      foo: false,
+    });
+    expect(callCount).toBe(3);
 
-  //   delete object.foo;
-  //   expect(callCount).toBe(4);
+    delete object.foo;
+    expect(callCount).toBe(4);
 
-  //   object.bar.a.b = 1;
-  //   expect(object.bar.a.b).toBe(1);
-  //   expect(callCount).toBe(5);
+    object.bar.a.b = 1;
+    expect(object.bar.a.b).toBe(1);
+    expect(callCount).toBe(5);
 
-  //   object.bar.a.c[2] = 5;
-  //   expect(object.bar.a.c[2]).toBe(5);
-  //   expect(callCount).toBe(6);
-  // });
+    object.bar.a.c[2] = 5;
+    expect(object.bar.a.c[2]).toBe(5);
+    expect(callCount).toBe(6);
+  });
 
   test('works with an array too', () => {
     const fixture = [1, 2, {
